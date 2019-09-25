9dbfa7324fd2da4e23727be4b9c9694fc2368c76
Michael <phamm89@gmail.com>
2019-09-25 08:44:57 -0400

    Unit Tests

---
+++ PrecipitationPredictionC-Sharp/PrecipitationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace PrecipitationPrediction
+{
+    [TestFixture]
+    public class PrecipitationTests
+    {
+        [Test]
+        public void PrecipitationGivenDate()
+        {
+            /* No Input */
+            string date = "9/21";
+
+            /* Expected Output */
+            string expectedOutput = $"\nThe predicted precipitation amount in the 27612 zipcode\nfor 9/21 is 0.135 units of precipitation.";
+
+            /* Predicted Output */
+            string predictedOutput = $"{PrecipitationOutput.Precipitation(date)}";
+
+            /* Check to see if equal */
+            Assert.AreEqual(expectedOutput, predictedOutput);
+        }
+
+        [Test]
+        public void PrecipitationNoDate()
+        {
+            /* No Input */
+            string date = "";
+
+            /* Expected Output */
+            string expectedOutput = $"\nThe predicted precipitation amount in the 27612 zipcode\nfor 9/25 is 0.56285714857143 units of precipitation.";
+
+            /* Predicted Output */
+            string predictedOutput = $"{PrecipitationOutput.Precipitation(date)}";
+
+            /* Check to see if equal */
+            Assert.AreEqual(expectedOutput, predictedOutput);
+        }
+    }
+}
--- PrecipitationPredictionC-Sharp/Program.cs
+++ PrecipitationPredictionC-Sharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace PrecipitationPrediction
 {
